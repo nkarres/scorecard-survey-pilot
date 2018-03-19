4ddba7252508a2325d0a02aaa13c5a01753a27b7
Unknown <nkarres@gmail.com>
2018-03-18 21:37:00 -0700

    Final clean up
    plus add firefox select fix

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 //surveyjs javascript
 Survey
     .StylesManager
-    // .applyTheme("default");
     .applyTheme("winterstone");
 
 var surveyJSON = { 
-	// cookieName: 'bf5b7f38-60fe-4ad9-b80a-a2e0e9652069', 
+    // cookieName: 'scorecard-survey-pilot', 
 	surveyId: 'bf5b7f38-60fe-4ad9-b80a-a2e0e9652069'
 }
-
-
-// send results dxService
-// var surveySendResult = function (s, options) {
-//     if (options.success) {
-//         s.getResult('7cafcce8-4bfb-4cbd-ac4d-84d1f1cd35ea', 'langs');
-//     }
-// };
 
 function sendDataToServer(survey) {
     survey.sendResult('f4362cfd-402d-40f6-a562-bb76aecf595a');
 }
 
 var survey = new Survey.Model(surveyJSON);
+
 $("#surveyContainer").Survey({
     model: survey,
     onComplete: sendDataToServer
 });
 
 
-// save local storage
+// save partial locally + send final result to dxService
 var storageName = "survey_water_funds_scorecard";
 function saveSurveyData(survey) {
     var data = survey.data;
@@ -76,7 +68,7 @@ survey.onAfterRenderPage.add(function(){
 $(document).ready(function(){
 	});
 
-// go to pages
+// nav bar page links
 $(function(){
 	$("nav li").eq(0).click(function () {
 	  survey.currentPageNo = 0;
@@ -117,75 +109,73 @@ $(function(){
 })
 
 
+// Guidance button for modal
+// jQuery.holdReady(true);
+// setTimeout(function(){ jQuery.holdReady(false); }, 500);
+// $(document).ready(function(){
+//     $("button#guide").click(function(){
+//         $("div.guide").slideToggle();
+//     });
+// });
+
+$(function(){
+   $("button.guide").click(function () {
+      $(this).text(function(i, text){
+          return text === "Guidance" ? "Hide Guidance" : "Guidance";
+      })
+   });
+})
 
 
-/*    jQuery.holdReady(true);
-    setTimeout(function(){ jQuery.holdReady(false); }, 500);
-    $(document).ready(function(){
-        $("button#guide").click(function(){
-            $("div.guide").slideToggle();
-        });
-    });*/
-    $(function(){
-       $("button.guide").click(function () {
-          $(this).text(function(i, text){
-              return text === "Guidance" ? "Hide Guidance" : "Guidance";
-          })
-       });
-    })
-
-
-
-//impacts opt groups
+// <optgroup> hack for impacts
 survey
     .onAfterRenderQuestion
     .add(function () {
-            opt1 = "Ecosystem function impacts";
-            opt2 = "Habitat impacts";
-            opt3 = "Biodiversity impacts";
-            opt4 = "Ecosystem benefit impacts";
-            opt5 = "Other beneficial impacts";
+        opt1 = "Ecosystem function impacts";
+        opt2 = "Habitat impacts";
+        opt3 = "Biodiversity impacts";
+        opt4 = "Ecosystem benefit impacts";
+        opt5 = "Other beneficial impacts";
 
-            $( "option:contains('" + opt1 + "')" ).replaceWith( "<optgroup label='" + opt1 + "'" );
-            $( "option:contains('" + opt2 + "')" ).before( "</optgroup>" );
-            
-            $( "option:contains('" + opt2 + "')" ).replaceWith( "<optgroup label='" + opt2 + "'" );
-            $( "option:contains('" + opt3 + "')" ).before( "</optgroup>" );
-            
-            $( "option:contains('" + opt3 + "')" ).replaceWith( "<optgroup label='" + opt3 + "'" );
-            $( "option:contains('" + opt4 + "')" ).before( "</optgroup>" );
+        $( "option:contains('" + opt1 + "')" ).replaceWith( "<optgroup label='" + opt1 + "'" );
+        $( "option:contains('" + opt2 + "')" ).before( "</optgroup>" );
+        
+        $( "option:contains('" + opt2 + "')" ).replaceWith( "<optgroup label='" + opt2 + "'" );
+        $( "option:contains('" + opt3 + "')" ).before( "</optgroup>" );
+        
+        $( "option:contains('" + opt3 + "')" ).replaceWith( "<optgroup label='" + opt3 + "'" );
+        $( "option:contains('" + opt4 + "')" ).before( "</optgroup>" );
 
-            $( "option:contains('" + opt4 + "')" ).replaceWith( "<optgroup label='" + opt4 + "'" );
-            $( "option:contains('" + opt5 + "')" ).before( "</optgroup>" );
+        $( "option:contains('" + opt4 + "')" ).replaceWith( "<optgroup label='" + opt4 + "'" );
+        $( "option:contains('" + opt5 + "')" ).before( "</optgroup>" );
 
-            $( "option:contains('" + opt5 + "')" ).replaceWith( "<optgroup label='" + opt5 + "'" );
-            $( "option:contains('" + opt5 + "')" ).after( "</optgroup>" );
+        $( "option:contains('" + opt5 + "')" ).replaceWith( "<optgroup label='" + opt5 + "'" );
+        $( "option:contains('" + opt5 + "')" ).after( "</optgroup>" );
     });
 
-//interventions opt groups
+// <optgroup> hack for interventions
 survey
     .onAfterRenderQuestion
     .add(function () {
-            opt1 = "Natural areas";
-            optLast1 = "Artificial areas";
-            opt3 = "Habitat Protection";
-            opt4 = "Habitat Management";
-            optLast2 = "Behavior Management";
+        opt1 = "Natural areas";
+        optLast1 = "Artificial areas";
+        opt3 = "Habitat Protection";
+        opt4 = "Habitat Management";
+        optLast2 = "Behavior Management";
 
-            $( "option:contains('" + opt1 + "')" ).replaceWith( "<optgroup label='" + opt1 + "'" );
-            $( "option:contains('" + optLast1 + "')" ).before( "</optgroup>" );
+        $( "option:contains('" + opt1 + "')" ).replaceWith( "<optgroup label='" + opt1 + "'" );
+        $( "option:contains('" + optLast1 + "')" ).before( "</optgroup>" );
 
-            $( "option:contains('" + optLast1 + "')" ).replaceWith( "<optgroup label='" + optLast1 + "'" );
-            $( "option:contains('" + optLast1 + "')" ).after( "</optgroup>" );
+        $( "option:contains('" + optLast1 + "')" ).replaceWith( "<optgroup label='" + optLast1 + "'" );
+        $( "option:contains('" + optLast1 + "')" ).after( "</optgroup>" );
 
-            $( "option:contains('" + opt3 + "')" ).replaceWith( "<optgroup label='" + opt3 + "'" );
-            $( "option:contains('" + opt4 + "')" ).before( "</optgroup>" );
+        $( "option:contains('" + opt3 + "')" ).replaceWith( "<optgroup label='" + opt3 + "'" );
+        $( "option:contains('" + opt4 + "')" ).before( "</optgroup>" );
 
-            $( "option:contains('" + opt4 + "')" ).replaceWith( "<optgroup label='" + opt4 + "'" );
-            $( "option:contains('" + optLast2 + "')" ).before( "</optgroup>" );
+        $( "option:contains('" + opt4 + "')" ).replaceWith( "<optgroup label='" + opt4 + "'" );
+        $( "option:contains('" + optLast2 + "')" ).before( "</optgroup>" );
 
-            $( "option:contains('" + optLast2 + "')" ).replaceWith( "<optgroup label='" + optLast2 + "'" );
-            $( "option:contains('" + optLast2 + "')" ).after( "</optgroup>" );
-
+        $( "option:contains('" + optLast2 + "')" ).replaceWith( "<optgroup label='" + optLast2 + "'" );
+        $( "option:contains('" + optLast2 + "')" ).after( "</optgroup>" );
     });
 
